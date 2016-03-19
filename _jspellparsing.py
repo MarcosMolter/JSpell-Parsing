c398af67c1f33bd615a89d856aa872cb3dc2d889
Marcos Molter <marcosmolter@gmail.com>
2016-03-18 21:23:04 -0300

    adicionado regra em dictionary.py para verbos irregulares modificado inflections para as novas regras de verbos irregulares
    Signed-off-by: Marcos Molter <marcosmolter@gmail.com>

--- _jspellparsing.py
+++ _jspellparsing.py
@@ -16,32 +16,38 @@ class jspell(object):
         self.files = utils.load_files()
         self.affixes = affixes.load_affix_dict(self.files.get('aff'))
         self.dictionary = dictionary.load_dictionary_dict(self.files.get('dic'))
-        
+        self.inflex = self.inflections()
     
     def inflections(self):
         inflection = {}        
         for dictword in self.dictionary.words:
-            inflection[dictword.word] = []
-            if dictword.affix is not None:
-                for word_affix in dictword.affix:
-                    flag_affix = self.affixes.affix[word_affix]
-                    for affix in flag_affix: 
-                        reg = re.search(affix.pattern % (affix.regex),dictword.word.upper())
-                        if reg is not None:
-                            if affix.displace is not None:
-                                p = re.sub(affix.pattern % (affix.displace),affix.replace,dictword.word.upper())
-                            else:
-                                p = affix.replace_str.format(dictword.word,affix.replace)
-                            inflection[dictword.word].append(p.decode('utf8').lower())                      
+            if dictword.irregular:
+                if dictword.word_irr not in inflection.keys():
+                   inflection[dictword.word_irr] = [] 
+                inflection[dictword.word_irr].append(dictword.word.decode('utf8').lower())
+            else:
+                inflection[dictword.word] = []
+                if dictword.affix is not None:
+                    for word_affix in dictword.affix:
+                        flag_affix = self.affixes.affix[word_affix]
+                        for affix in flag_affix: 
+                            reg = re.search(affix.pattern % (affix.regex),dictword.word.upper())
+                            if reg is not None:
+                                if affix.displace is not None:
+                                    p = re.sub(affix.pattern % (affix.displace),affix.replace,dictword.word.upper())
+                                else:
+                                    p = affix.replace_str.format(dictword.word,affix.replace)
+                                inflection[dictword.word].append(p.decode('utf8').lower())                      
 
         return inflection
     
-    def lemmatize(self,lex,w):
+    def lemmatize(self,w):
+        lex = self.inflex       
         find = [k for (k,v) in lex.iteritems() if w.decode('utf8') in v]
         if find == []:
             return w
         else:
-            return find
+            return find[0]
             
 
         
--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,9 @@ Created on Thu Mar 17 20:11:36 2016
 import re
 
 dictionary_pattern = dict(
-    word = '^(?P<word>[^\/]+)[\/](?P<tag>[^\/]+)[\/]?(?P<afix>[^\/]+)?[\/]$'
+    word = '^(?P<word>[^\/]+)[\/](?P<tag>[^\/]+)[\/]?(?P<afix>[^\/]+)?[\/]$',
+    word_irr = '^#(?P<code>\d{3})\/(?P<word>\D+)\n$',
+    irregular =  '^(?P<word>[^\/]+)[\/\$\#]+(?P<code>[\d]+)[\$#]+\w{1}[\$#]+(?P<tag>[\d]+)\/\w$'   
     )
 
 
@@ -17,15 +19,26 @@ class Word:
         self.word = re_search_word.get('word')
         self.tag = re_search_word.get('tag')
         self.affix = re_search_word.get('afix')
+        self.irregular = False
+        self.word_irr = None
        
 class Dictionary:
     
     def __init__(self):
         self.words = []
-
+        self.word_irr = None
         
     def f_word(self,re_search):
         self.words.append(Word(re_search.groupdict()))
+
+    def f_word_irr(self,re_search):
+        self.word_irr = re_search.groupdict().get('word')
+        
+    def f_irregular(self,re_search):
+        word = Word(re_search.groupdict())
+        word.irregular = True
+        word.word_irr = self.word_irr        
+        self.words.append(word)        
 
         
 
@@ -39,7 +52,9 @@ def load_dictionary_dict(aff_file_path):
     
     dictionary = Dictionary()
     dictionary_keys = [
-        Dictionarykey(dictionary.f_word, 'word')
+        Dictionarykey(dictionary.f_word, 'word'),
+        Dictionarykey(dictionary.f_word_irr,'word_irr'),
+        Dictionarykey(dictionary.f_irregular, 'irregular')
         ]
         
     for line in dic_file:
